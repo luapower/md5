d8150508a5c28d7b980285c442b0b4f3edb4c20a
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2014-01-23 00:52:03 +0200

    removed CFLAGS/CXXFLAGS

--- csrc/md5/build-linux32.sh
+++ csrc/md5/build-linux32.sh
@@ -1,3 +1,3 @@
-gcc $CFLAGS md5.c -shared -o ../../bin/linux32/libmd5.so -I.
+gcc -O2 -s -static-libgcc md5.c -shared -o ../../bin/linux32/libmd5.so -I.
 
 #cd ../.. && linux/bin/luajit md5_test.lua
