2d52cb323fb013f819f1d0abd887866f5b43a759
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-03-26 20:08:31 +0200

    rebuild for osx with @loader_path-based install_name

--- csrc/md5/build-osx32.sh
+++ csrc/md5/build-osx32.sh
@@ -1,1 +1,1 @@
-gcc -arch i386 -O2 md5.c -shared -o ../../bin/osx32/libmd5.dylib -I.
+gcc -arch i386 -O2 md5.c -shared -install_name @loader_path/libmd5.dylib -o ../../bin/osx32/libmd5.dylib -I.
--- csrc/md5/build-osx64.sh
+++ csrc/md5/build-osx64.sh
@@ -1,1 +1,1 @@
-gcc -arch x86_64 -O2 md5.c -shared -o ../../bin/osx64/libmd5.dylib -I.
+gcc -arch x86_64 -O2 md5.c -shared -install_name @loader_path/libmd5.dylib -o ../../bin/osx64/libmd5.dylib -I.
