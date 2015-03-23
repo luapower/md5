fabb80e41bef0722eddbc4cae327db9b1de08105
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2015-03-24 00:03:53 +0200

    unimportant

--- csrc/md5/build-osx64.sh
+++ csrc/md5/build-osx64.sh
@@ -1,2 +1,2 @@
-P=osx64 C="-arch x86_64" L="-arch x86_64 -install_name @loader_path/libmd5.dylib" \
+P=osx64 C="-arch x86_64" L="-arch x86_64 -install_name @rpath/libmd5.dylib" \
 	D=libmd5.dylib A=libmd5.a ./build.sh
